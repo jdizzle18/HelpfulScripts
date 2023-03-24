8de64839ab88270d685353043c29d6c5d0cee473
J.D <51488229+jdizzle18@users.noreply.github.com>
2023-03-24 14:01:15 -0400

    Add files via upload

---
+++ ReportCertificates.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module Webadministration
+Cls
+$env:COMPUTERNAME | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt"
+Write-Host "List of IIS Sites and Bindings"  | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
+Write-Host "==============================" | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
+Get-ChildItem -Path IIS:\Sites | format-table  | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
+Write-Host "List of Certificates and Exp Dates" | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
+Write-Host "==================================" | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
+Get-ChildItem Cert:\LocalMachine\My | format-table -Property Subject, NotAfter, Thumbprint  | Out-File -Filepath $env:COMPUTERNAME"_ReportCertificates.txt" -Append
